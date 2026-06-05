fc0e1b64865b9f31b68404144194ad8091b4f374
MSHND <193864085+MSHND@users.noreply.github.com>
2026-06-05 22:36:30 +0800

    Add dormant boot load manifest scaffold

---
+++ js/boot/pocket-load-manifest.js
@@ -0,0 +1,14 @@
+/* Pocket boot manifest scaffold.
+   Dormant until index.html loads js/boot/pocket-boot.js. */
+(function (global) {
+  "use strict";
+
+  global.PocketLoadManifest = Object.freeze({
+    core: [],
+    tree: [],
+    commands: [],
+    pe: [],
+    health: [],
+    legacyTransition: []
+  });
+})(window);
